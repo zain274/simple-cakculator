82079d650a10d53758f68df31804151b9ce53a0f
zain raza <zrabbas110@gmail.com>
2024-06-04 19:27:34 +0500

    simple cal assigment

---
+++ index.ts
@@ -0,0 +1,54 @@
+#!  /usr/bin/env node
+import inquirer from "inquirer";
+import Choice from "inquirer/lib/objects/choice.js";
+
+const answer= await inquirer.prompt(
+    [
+        {
+            message:"Enter First Number",
+            type:"number",
+            name:"Firstnumber",
+
+        },
+        {
+            message:"Enter Second Number",
+            type:"number",
+            name:"Secondnumber"
+        },
+        {
+            message:"Select one of the operators to perfrom operation",
+            type:"list",
+            name:"operaots",
+            choices:["Addition","Subtraction","Multiplication","Division"]
+
+        }
+    ]
+);
+if(answer.operaots==="Addition"){
+    console.log(`your value is ${answer.Firstnumber+answer.Secondnumber}`);
+    
+}
+else if(answer.operaots==="Subtraction"){
+    console.log(`your value is ${answer.Firstnumber-answer.Secondnumber}`);
+
+}
+
+else if(answer.operaots==="Multiplication"){
+    console.log(`your value is ${answer.Firstnumber*answer.Secondnumber}`);
+
+}
+
+
+else if(answer.operaots==="Division"){
+    console.log(`your value is ${answer.Firstnumber/answer.Secondnumber}`);
+
+}
+else{
+    console.log("please select valid opertors");
+    
+}
+
+
+
+
+// console.log(answer);
